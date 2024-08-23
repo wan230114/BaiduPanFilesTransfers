e1f0a115a153e81568495d1b9343e312b0072b49
wan230114 <1170101471@qq.com>
2024-08-23 14:11:20 +0800

    接口优化

--- share_link.py
+++ share_link.py
@@ -28,6 +28,8 @@ class share_baidu_link():
     def handle_list_dir(self) -> None:
         """获取目标目录下的文件和目录列表"""
         self.dir_list_all = self.network.get_dir_list(f'/{self.folder_name}')
+        from pprint import pprint
+        pprint(list(map(lambda x: (x["isdir"], x["path"]), self.dir_list_all)))
         if isinstance(self.dir_list_all, int) or not self.dir_list_all:
             print(f'目录 {self.folder_name} 中没获取到任何内容，请求返回：{self.dir_list_all}')
 
@@ -36,26 +38,26 @@ class share_baidu_link():
         for info in self.dir_list_all:
             self.process_share(info)
 
-    def process_share(self, info: dict) -> None:
+    def process_share(self, info: dict) -> str:
         """执行分享操作并记录结果"""
         # 插入要分享的文件或文件夹到链接输入框，对文件夹加入 "/" 标记来区别
         is_dir = "/" if info["isdir"] == 1 else ""
         filename = f"{info['server_filename']}{is_dir}"
-        msg = f'目录：{filename}' if is_dir else f'文件：{filename}'
-        print(msg)
 
         # 发送创建分享请求
+        # msg = f'目录：{filename}' if is_dir else f'文件：{filename}'
         r = self.network.create_share(info['fs_id'], EXP_MAP[self.expiry], self.password)
         if isinstance(r, str):
             result = f'链接：{r}?pwd={self.password}，密码：{self.password}，名称：{filename}'
         else:
             result = f'分享失败：错误代码（{r}），名称：{filename}'
         print(result)
+        return result
 
 
-CONFIG_PATH = "config.ini"
-CONFIGs = [line.strip() for line in open(CONFIG_PATH).readlines()]
+if __name__ == "__main__":
+    CONFIG_PATH = "config.ini"
+    CONFIGs = [line.strip() for line in open(CONFIG_PATH).readlines()]
 
-share_baidu_link(CONFIGs[0], CONFIGs[1])
-
+    share_baidu_link(CONFIGs[0], CONFIGs[1])
 
